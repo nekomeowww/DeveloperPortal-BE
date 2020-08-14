ad9bfb1e735f184e30c5c54d44c47e7910d90d94
Ayaka Neko <neko@ayaka.moe>
2020-08-14 21:29:58 +0800

    :art: 调整了两个函数调用方式
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/login.js
+++ src/api/login.js
@@ -4,7 +4,7 @@ const KoaRouter = require('koa-router')
 // Local Package
 const login = require('../controllers/login')
 
-const LoginRouter = new KoaRouter
+const LoginRouter = new KoaRouter()
 
 LoginRouter.get('/', login)
 
