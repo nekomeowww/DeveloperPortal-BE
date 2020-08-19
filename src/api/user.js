2b6690551d8c1a42dbc2eb1d9f44e6d34584ac76
Ayaka Neko <neko@ayaka.moe>
2020-08-19 22:07:49 +0800

    :sparkles: 添加了 Team App 上传的接口
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/user.js
+++ src/api/user.js
@@ -7,6 +7,7 @@ const getAvatar = require('../controllers/avatar')
 const { app, uploadAppIcon } = require('../controllers/app')
 const { team, uploadTeamIcon } = require('../controllers/team')
 const { avatar, nickname, email } = require('../controllers/update')
+const { uploadTeamAppIcon } = require('../controllers/team/app')
 
 let user = new KoaRouter()
 
@@ -19,5 +20,6 @@ user.get("/update/avatar", avatar)
 user.get("/update/nickname", nickname)
 user.post("/:userId/app/:id/uploadAppIcon", uploadAppIcon)
 user.post("/:userId/team/:id/uploadTeamIcon", uploadTeamIcon)
+user.post("/:userId/team/:id/app/:appId/uploadTeamAppIcon", uploadTeamAppIcon)
 
 module.exports = user
