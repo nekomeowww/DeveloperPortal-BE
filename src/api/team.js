3bad8fb53b6b8d3303d767fa60b64d9a0b48371b
Ayaka Neko <neko@ayaka.moe>
2020-08-19 22:09:35 +0800

    :sparkles: 对接了增删减改接口
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/team.js
+++ src/api/team.js
@@ -3,6 +3,7 @@ const KoaRouter = require('koa-router')
 
 // Local Package
 const { team, newTeam, removeTeam, getTeamIcon, getTeamDetail } = require('../controllers/team')
+const { newTeamApp, getTeamAppSecret, removeTeamApp, getTeamAppIcon, resetTeamSecret, teamApp, getTeamAppDetail } = require('../controllers/team/app')
 
 const TeamRouter = new KoaRouter()
 
@@ -14,5 +15,15 @@ TeamRouter.post("/new", newTeam)
 TeamRouter.get("/remove", removeTeam)
 // Team Icon Control
 TeamRouter.get("/teamIcon", getTeamIcon)
+// Team Application Related
+TeamRouter.get("/app", teamApp)
+TeamRouter.get("/appdetail", getTeamAppDetail)
+TeamRouter.get("/secret", getTeamAppSecret)
+TeamRouter.get("/resetsecret", resetTeamSecret)
+// Team Creation and deletion
+TeamRouter.post("/newapp", newTeamApp)
+TeamRouter.get("/removeapp", removeTeamApp)
+// Team Icon Control
+TeamRouter.get("/appIcon", getTeamAppIcon)
 
 module.exports = TeamRouter
