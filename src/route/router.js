9874642b906e13bb857fff4df7389189c5db29a3
Ayaka Neko <neko@ayaka.moe>
2020-08-20 20:55:07 +0800

    :sparkles: 添加了 invite 和 notification 两个接口的对接
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/route/router.js
+++ src/route/router.js
@@ -7,11 +7,15 @@ let img = require('../api/img')
 let user = require('../api/user')
 let team = require('../api/team')
 let login = require('../api/login')
+let invite = require('../api/invite')
+let notification = require('../api/notification')
 
 routers.use("/app", app.routes(), app.allowedMethods())
 routers.use("/login", login.routes(), login.allowedMethods())
+routers.use("/invite", invite.routes(), invite.allowedMethods())
 routers.use("/user", user.routes(), user.allowedMethods())
 routers.use("/team", team.routes(), team.allowedMethods())
 routers.use("/img", img.routes(), img.allowedMethods())
+routers.use("/notification", notification.routes(), notification.allowedMethods())
 
 module.exports = routers
