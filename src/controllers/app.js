3489057715198561fd97c33fcc0901df43ed5155
Ayaka Neko <neko@ayaka.moe>
2020-08-25 16:42:49 +0800

    :art: 调整 callback url 的限制，现在只允许同域名跳转，添加基本信息获取接口
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/controllers/app.js
+++ src/controllers/app.js
@@ -33,11 +33,15 @@ let newApp = async (ctx, next) => {
     clientId = clientId.join('')
     clientSecret = Hash.sha256(Date.now() + '').substring(0, 16)
 
+    if (!/https?:\/\//.test(body.form.callback)) {
+        body.form.callback = 'https://' + body.form.callback
+    }
+
     if (!isExist) {
 
         const appId = Hash.sha256(Date.now() + '').substring(0, 16)
 
-        await Store.user.insert({ key: "AppProfileSecret", appId: appId, userId: parseInt(body.userId), clientId: clientId, clientSecret: clientSecret})
+        await Store.user.insert({ key: "AppProfileSecret", appId: appId, userId: parseInt(body.userId), clientId: clientId, clientSecret: clientSecret })
         await Store.user.insert({ key: "AppProfile", appId: appId, userId: parseInt(body.userId), detail: body.form })
         await Store.user.update({ key: "AppProfiles", id: parseInt(body.userId) }, { $addToSet: { apps: appId } }, {})
 
@@ -85,12 +89,12 @@ let removeApp = async (ctx, next) => {
         let apps = appProfile.apps.filter(id => id !== query.appId)
         await Store.user.update({ key: "AppProfiles", id: parseInt(query.userId) }, { $set: { apps: apps } }, {})
         appProfile = await Store.user.findOne({ key: "AppProfiles", id: parseInt(query.userId) })
-        Log.debug('after: ' + JSON.stringify(appProfile))        
+        Log.debug('after: ' + JSON.stringify(appProfile))
         ctx.body = { code: 0, message: "success", appId: query.appId }
         await next()
     }
     else {
-        ctx.body = { code: 1, message: "nothing to remove", appId: query.appId}
+        ctx.body = { code: 1, message: "nothing to remove", appId: query.appId }
         await next()
     }
 }
@@ -144,6 +148,21 @@ let resetSecret = async (ctx, next) => {
     }
 }
 
+let getAppBasic = async (ctx, next) => {
+    let query = ctx.request.query
+    query = JSON.parse(JSON.stringify(query))
+
+    if (query.appId === undefined || query.appId === "undefined" || query.appId === "null") {
+        ctx.body = { status: "failed", message: "Invalid Request, Missing value on required field `appId`" }
+        await next()
+        return
+    }
+
+    let app = await Store.user.findOne({ key: "AppProfile", appId: query.appId })
+    ctx.body = app
+    await next()
+}
+
 let getAppDetail = async (ctx, next) => {
     let query = ctx.request.query
     query = JSON.parse(JSON.stringify(query))
@@ -159,7 +178,7 @@ let getAppDetail = async (ctx, next) => {
     let instance = await Store.user.findOne({ key: "InstanceProfile", appId: query.appId })
     if (instance) app.detail.callback = instance.callback
     await Store.user.remove({ key: "InstanceProfile", appId: query.appId }, {})
-    
+
     ctx.body = app
     await next()
 }
@@ -248,9 +267,27 @@ let postOauth = async (ctx, next) => {
     let appId = appSecret.appId
     let app = await Store.user.findOne({ key: "AppProfile", appId: appId })
     if (app) {
-        if (body.redirect_uri) await Store.user.insert({ key: "InstanceProfile", callback: body.redirect_uri, appId: appId, clientId: body.clientId, clientSecret: appSecret.clientSecret })
-        ctx.body = { code: 0, message: "success" }
-        await next()
+        if (!/https?:\/\//.test(app.detail.callback)) {
+            let callback = 'https://' + app.detail.callback
+            let url = new URL(callback)
+            if (body.redirect_uri) {
+                let newCallback = body.redirect_uri
+                if (!/https?:\/\//.test(body.redirect_uri)) {
+                    newCallback = 'https://' + body.redirect_uri
+                }
+                let newUrl = new URL(newCallback)
+
+                if (url.hostname === newUrl.hostname) {
+                    await Store.user.insert({ key: "InstanceProfile", callback: body.redirect_uri, appId: appId, clientId: body.clientId, clientSecret: appSecret.clientSecret })
+                    ctx.body = { code: 0, message: "success" }
+                    await next()
+                }
+                else {
+                    ctx.body = { code: 2, message: "Failed with hostname mismatch, redirect to cross site is permitted."}
+                    await next()
+                }
+            }
+        }
     } else {
         ctx.body = { code: 1, message: "no valid app found" }
         await next()
@@ -262,6 +299,7 @@ module.exports = {
     getAppIcon,
     newApp,
     removeApp,
+    getAppBasic,
     getAppDetail,
     getAppSecret,
     resetSecret,
